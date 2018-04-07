7089d53ccb66a1200be9789fa220c339672ef5be
Anurag <anurag.mishra@fundingsocieties.com>
2018-04-07 14:05:16 +0800

    Adding code for dummy data

--- src/app/home/content/facebook/facebook.component.ts
+++ src/app/home/content/facebook/facebook.component.ts
@@ -6,8 +6,120 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./facebook.component.css']
 })
 export class FacebookComponent {
-
+    data: any = [];
     constructor() {
+
+    }
+
+    ngOnInit() {
+        this.getData()
+    }
+
+    getData() {
+        this.data.push({
+            id: 1,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 2,
+            medium: '20mdief',
+            campaign: 'FB_ADS_3',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 3,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 4,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 5,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 6,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 7,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 8,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 9,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 10,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 11,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 12,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
+        this.data.push({
+            id: 13,
+            medium: 'dont know',
+            campaign: 'FB_ADS',
+            term: 'SOMETHING_TERM',
+            content: 'this is sparta',
+            others: '{{id : block}}'
+        });
 
     }
 
