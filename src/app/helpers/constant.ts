ce2e40952686d8738e370977cee4a8df6c3f7dd9
Anurag <anurag.mishra@fundingsocieties.com>
2018-04-14 00:07:44 +0800

    Adding new url

--- src/app/helpers/constant.ts
+++ src/app/helpers/constant.ts
@@ -1,2 +1,3 @@
-export const TRACKER_ADDRESS = 'https://ec2-13-59-135-145.us-east-2.compute.amazonaws.com/';
+// export const TRACKER_ADDRESS = 'https://ec2-13-59-135-145.us-east-2.compute.amazonaws.com/';
+export const TRACKER_ADDRESS ='https://trackbalancer-1506384121.us-east-2.elb.amazonaws.com/';
 // export const TRACKER_ADDRESS = 'http://localhost:3000/';
