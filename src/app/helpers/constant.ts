641a209ae5a56405e5cbe8f74e0697def34792c5
Anurag <anurag.mishra@fundingsocieties.com>
2018-04-14 01:57:10 +0800

    Adding code for heroku service

--- src/app/helpers/constant.ts
+++ src/app/helpers/constant.ts
@@ -1,2 +1,3 @@
-export const TRACKER_ADDRESS = 'http://ec2-13-59-135-145.us-east-2.compute.amazonaws.com/';
+export const AWS_ADDRESS = 'http://ec2-13-59-135-145.us-east-2.compute.amazonaws.com/';
+export const TRACKER_ADDRESS = 'https://whispering-escarpment-11943.herokuapp.com/';
 // export const TRACKER_ADDRESS = 'http://localhost:3000/';
